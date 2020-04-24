8129cbc1652a2990514411491aa071c4b14f7c3c
Jamison Cozart <jamisoncozart@gmail.com>
2020-04-24 11:57:54 -0700

    Update NewCoffeeFrom component to create new Coffee object and add to state

--- src/Components/Body/NewCoffeeForm/NewCoffeeForm.js
+++ src/Components/Body/NewCoffeeForm/NewCoffeeForm.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { v4 } from 'uuid';
 
 const NewCoffeeForm = props => {
+
+  function handleNewBrewSubmit(event) {
+    event.preventDefault();
+    props.onNewBrewCreation({
+      id: v4(),
+      name: event.target.Name.value,
+      description: event.target.Description.value,
+      image: event.target.Image.value,
+      quantity: 20
+    });
+  }
+
   return (
-    <form className="NewCoffeeForm">
+    <form onSubmit={handleNewBrewSubmit} className="NewCoffeeForm">
       <h2>Add New Brew</h2>
       <input placeholder="Brew Name" name="Name" />
       <input placeholder="Description" name="Description" />
+      <input placeholder="Image URL" name="Image" />
       <button>Add Brew</button>
     </form>
   )
