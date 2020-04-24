ac3ca6b5672d3f6a9e08045815673af86cff71f6
Jamison Cozart <jamisoncozart@gmail.com>
2020-04-24 11:54:19 -0700

    Update App.js to render Header, Body, and Footer components

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,16 +3,14 @@ import Header from './Header/Header';
 import Body from './Body/Body';
 import Footer from './Footer/Footer';
 
-class App extends React.Component {
-  render() {
-    return (
-      <React.Fragment>
-        <Header />
-        <Body />
-        <Footer />
-      </React.Fragment>
-    )
-  }
+const App = props => {
+  return (
+    <React.Fragment>
+      <Header />
+      <Body />
+      <Footer />
+    </React.Fragment>
+  )
 }
 
 export default App;
