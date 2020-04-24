cf9b4d9788c69c2f6d9c473785504e1af12c2ff4
Jamison Cozart <jamisoncozart@gmail.com>
2020-04-24 11:57:24 -0700

    Add BrewDetails component to render individual Coffee details

--- src/Components/Body/BrewDetails/BrewDetails.js
+++ src/Components/Body/BrewDetails/BrewDetails.js
@@ -3,9 +3,10 @@ import React from 'react';
 const BrewDetails = props => {
   return (
     <div className="BrewDetails">
-      <img src="https://i.ya-webdesign.com/images/in-the-clipart-coffee-1.png" />
-      <p className="BrewDescription"> Elit irure esse in sunt. Irure id tempor esse ad excepteur esse eu ullamco dolor pariatur et voluptate.</p>
-      <button>Back to Brews</button>
+      <img src={props.image} />
+      <h2>{props.name}</h2>
+      <p className="BrewDescription">{props.description}</p>
+      <button onClick={props.handleBackToMainPage}>Back to Brews</button>
     </div>
   )
 }
