9a5d31475eebecd536de99838c7919c79c8a1cb3
Jamison Cozart <jamisoncozart@gmail.com>
2020-04-24 15:33:33 -0700

    Add onClick functionality for all buttons within BrewButtons component

--- src/Components/Body/Coffee/BrewButtons/BrewButtons.js
+++ src/Components/Body/Coffee/BrewButtons/BrewButtons.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
 const BrewButtons = props => {
+
+  function handleClickPropogation(event) {
+    return props.handleRefillingCoffeeQuantity(props.id);
+  }
+
   return (
     <div className="BrewButtons">
-      <button>Refill</button>
-      <p className="BrewAmount">{props.quantity}</p>
-      <p className="cups">Cups</p>
-      <button>Tap</button>
+      <button onClick={handleClickPropogation}>Refill</button>
+      <div className="BrewQuantity">
+        <p className="BrewAmount">{props.quantity}</p>
+        <p className="cups">Cups</p>
+      </div>
+      <button onClick={() => props.handleDecrementingCoffeeQuantity(props.id)}>Tap</button>
     </div>
   )
 }
