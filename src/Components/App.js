009201975b3df2b4d1c478f0686f04ac2d99306c
Jamison Cozart <jamisoncozart@gmail.com>
2020-04-24 15:32:21 -0700

    Add state changes for quantity

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header/Header';
 import Body from './Body/Body';
 import Footer from './Footer/Footer';
+import '../App.css';
 
 const App = props => {
   return (
