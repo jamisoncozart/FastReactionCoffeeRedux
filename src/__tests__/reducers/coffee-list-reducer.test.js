44043d22a9d48ce7ace5e47c9283497887484a81
jamisoncozart <jamisoncozart@gmail.com>
2020-05-01 08:56:31 -0700

    Test for adding coffee to coffeeList

--- src/__tests__/reducers/coffee-list-reducer.test.js
+++ src/__tests__/reducers/coffee-list-reducer.test.js
@@ -4,4 +4,20 @@ describe('coffeeListReducer', () => {
   test('if default state is returned when no action type is given', () => {
     expect(coffeeListReducer(undefined, { type: null })).toEqual({});
   });
+  test('if a new coffee is added to the list', () => {
+    const action = {
+      type: 'ADD_COFFEE',
+      id: '1',
+      name: 'Noggaccino',
+      description: 'Smooth eggnog with hints of nutmeg and cinammon',
+      image: 'https://www.laweekly.com/wp-content/uploads/2019/05/xmascoffee_menottis.jpg'
+    }
+    expect(coffeeListReducer(undefined, action)).toEqual({
+      '1': {
+        name: action.name,
+        description: action.description,
+        image: action.image
+      }
+    });
+  });
 });
